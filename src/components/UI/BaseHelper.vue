852c98ef92695d09896ea11feae610cc4832def9
spoonty <sergey.kachalov.02@gmail.com>
2022-11-08 10:43:53 +0700

    Create BaseHelpter component

---
+++ src/components/UI/BaseHelper.vue
@@ -0,0 +1,26 @@
+<template>
+    <div
+        class="base-helper"
+    >
+        {{ innerText }}
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from "vue"
+
+defineProps({
+    innerText: String
+})
+</script>
+
+<style lang="scss" scoped>
+.base-helper {
+    position: absolute;
+    padding: 3px 5px;
+    font-size: 12px;
+    color: $bg;
+    background-color: $base2;
+    border-radius: 10px;
+}
+</style>
--- src/components/WalletPage/WalletDataItem.vue
+++ src/components/WalletPage/WalletDataItem.vue
@@ -4,18 +4,38 @@
             {{ title }}
         </div>
         <div
+            v-if="!toCopy"
             class="item-value"
-            :class="{ 'to-copy': toCopy }"
-            @click="copy"
         >
             {{ dataItem }}
+        </div>
+        <div
+            v-else
+            class="item-value"
+        >
+            <span
+                style="cursor: pointer"
+                @click="copy"
+                @mouseover="isHovered = true"
+                @mouseleave="mouseLeaveAction"
+            >
+                {{ dataItem }}
+            </span>
+            <transition name="helper-transition">
+                <base-helper
+                    v-if="isHovered"
+                    :inner-text="innerText"
+                    style="margin-top: 20px"
+                />
+            </transition>
         </div>
     </div>
 </template>
 
 <script lang="ts" setup>
-import { defineProps } from "vue"
+import { ref, defineProps } from "vue"
 import copyText from "@/helpers/copyText"
+import BaseHelper from "@/components/UI/BaseHelper.vue"
 
 const props = defineProps({
     title: String,
@@ -26,10 +46,20 @@ const props = defineProps({
     }
 })
 
+const DEFAULT_INNER_TEXT = "Click to copy"
+let innerText = ref(DEFAULT_INNER_TEXT)
+let isHovered = ref(false)
+
 function copy() {
     if (props.toCopy) {
         copyText(props.dataItem)
+        innerText.value = "Copied!"
     }
+}
+
+function mouseLeaveAction() {
+    isHovered.value = false
+    innerText.value = DEFAULT_INNER_TEXT
 }
 </script>
 
@@ -46,8 +76,20 @@ function copy() {
 }
 .item-value {
     font-family: "Gilroy Regular";
+    display: flex;
+    justify-content: center;
 }
-.to-copy {
-    cursor: pointer;
+
+.helper-transition-item {
+    display: inline-block;
+}
+.helper-transition-enter-active,
+.helper-transition-leave-active {
+    transition: all .5s ease;
+}
+.helper-transition-enter-from,
+.helper-transition-leave-to {
+    opacity: 0;
+    transform: translateY(10px);
 }
 </style>
