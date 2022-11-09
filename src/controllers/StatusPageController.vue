7f89422a2ed8f0c77c6e25d177ff24fbb03ea9e7
spoonty <sergey.kachalov.02@gmail.com>
2022-11-09 13:26:12 +0700

    Add getting link from hash

--- src/controllers/StatusPageController.vue
+++ src/controllers/StatusPageController.vue
@@ -4,6 +4,19 @@
 
 <script setup>
 import StatusPage from "@/pages/StatusPage"
+import { ref, onMounted } from "vue"
+import { useRoute, useRouter } from "vue-router"
+import { GOERLI_ETHERSCAN } from "@/const/URL"
+
+const route = useRoute()
+const router = useRouter()
+
+let link = ref(GOERLI_ETHERSCAN)
+
+onMounted(() => {
+    link.value += route.params.hash
+    console.log(link.value)
+})
 </script>
 
 <style scoped>
