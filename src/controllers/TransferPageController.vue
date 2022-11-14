d68d1152170be3005fd04b372c20f9cf1e44bb83
spoonty <sergey.kachalov.02@gmail.com>
2022-11-14 16:38:36 +0700

    Fix package.json fole

--- src/controllers/TransferPageController.vue
+++ src/controllers/TransferPageController.vue
@@ -51,7 +51,14 @@ function redirectWalletPage() {
 let notification = ref("")
 
 async function transferCurrency() {
-    const connection = ConnectionFactory.GetWalletConnection(WalletType.META_MASK)
+    let connection = ConnectionFactory.GetWalletConnection(WalletType.META_MASK)
+
+    if (connection.status === -1) {
+        notification.value = connection.description
+        return
+    }
+
+    connection = connection.description
     const provider = connection.getProvider()
     const signer = await provider.getSigner()
     const address = await signer.getAddress()
